0e4a8cfc47da88bc1d822c93fec92d03a8464b17
Prasanthi Relangi <prasanthi.relangi@gmail.com>
2016-06-18 14:51:41 -0700

    Add animation and dice selection

--- Features WatchKit Extension/FeatureInterfaceController.swift
+++ Features WatchKit Extension/FeatureInterfaceController.swift
@@ -12,8 +12,17 @@ import Foundation
 
 class FeatureInterfaceController: WKInterfaceController {
 
+    @IBOutlet var diceImage: WKInterfaceImage!
+    @IBOutlet var rollButton: WKInterfaceButton!
+    @IBOutlet var diceGroup: WKInterfaceGroup!
+    @IBOutlet var diceRollGroup: WKInterfaceGroup!
+    @IBOutlet var diceRollImage: WKInterfaceImage!
+    
     override func awake(withContext context: AnyObject?) {
         super.awake(withContext: context)
+        
+        diceRollImage.setImageNamed("frame_")
+        //diceRollImage.startAnimatingWithImages(in: NSRange(location: 0, length: 8), duration: 5, repeatCount: Int.max)
         
         // Configure interface objects here.
     }
@@ -28,4 +37,23 @@ class FeatureInterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+    @IBAction func onStop() {
+        diceRollImage.stopAnimating()
+        
+        let diceNumber = arc4random()%5
+        let diceImageName: String = "\(diceNumber+1)"
+        print("diceImageName: \(diceImageName)")
+        diceRollImage.setImageNamed(diceImageName)
+        
+    }
+    
+    @IBAction func onRoll() {
+        print("During the dice roll")
+        diceRollImage.setImageNamed("frame_")
+        diceRollImage.startAnimatingWithImages(in: NSRange(location: 0, length: 12), duration: 0.8, repeatCount: Int.max)
+        //diceRollImage.stopAnimating()
+        
+        
+        
+    }
 }
